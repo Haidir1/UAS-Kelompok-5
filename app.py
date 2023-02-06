dcf0c5dbe319c5140a5bc60ece38712360ae66a3
Haidir <30haidir@gmail.com>
2023-02-07 00:12:29 +0700

    test

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ scaler = joblib.load("scaler.save")
 
 
 app = Flask(__name__)
-model = pickle.load(open('modelfixRF.pkl', 'rb'))
+model = pickle.load(open('modelRF.pkl', 'rb'))
 
 
 @app.route("/home")
@@ -33,7 +33,7 @@ def predict():
         else:
             prediction = "Tidak Aman"
 
-        return render_template('output.html', prediction_text="Air {} Untuk Dikonsumsi ".format(prediction), ph=features_value[0][0], Hardness=features_value[0][1],Solids=features_value[0][2], Chloramines=features_value[0][3], Sulfate=features_value[0][4], Conductivity=features_value[0][5], Organic_carbon=features_value[0][6], Trihalomethanes=features_value[0][7], Turbidity=features_value[0][8])
+        return render_template('output.html', prediction_text="Air {} Untuk Dikonsumsi ".format(prediction), ph=features_value[0][0], Hardness=features_value[0][1], Solids=features_value[0][2], Chloramines=features_value[0][3], Sulfate=features_value[0][4], Conductivity=features_value[0][5], Organic_carbon=features_value[0][6], Trihalomethanes=features_value[0][7], Turbidity=features_value[0][8])
 
 
 if __name__ == "__main__":
